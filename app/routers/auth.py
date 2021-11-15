cb014a5de801654fc1688241bc77a26b7fea3221
thierno <thiernoibrahimalo@esp.sn>
2021-11-15 21:28:56 +0000

    configuring testing database

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -16,10 +16,10 @@ def login( user_credentials : OAuth2PasswordRequestForm = Depends(), db:Session 
     user=db.query(models.User).filter(
         models.User.username==user_credentials.username).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND ,details=f"Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND )
 
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     #create token
     access_token =oauth2.create_access_token(data={"user_id":user.id})
     return {"access token":access_token, "token type":"Bearer"}
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,13 +1,40 @@
-from fastapi.testclient import TestClient
-from app.main import app
+import pytest
+from jose import jwt
+from app import schemas
+from .database import client,session
+from app.config import settings
 
-client =TestClient(app)
 
-def test_root():
-    res=client.get("/")
-    assert res.json().get('Hello')=="Worst day of my life"
-    assert res.status_code==200
-    
-def test_create_user():
-    res=client.post("/api/v1/users/",json={"username": "thiere","email": "email@email.com", "password": "password"})
-    print(res.json())
+def test_create_user(client):
+    res = client.post(
+        "/api/v1/users/", json={"username":"thiere","email": "hello123@gmail.com", "password": "password123"})
+
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
+
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/api/v1/login", data={"username": test_user['username'], "password": test_user['password'],"email": test_user['email']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
+
+
+@pytest.mark.parametrize("username, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('sanjeev@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+
+])
+def test_incorrect_login(test_user, client, username, password, status_code):
+    res = client.post(
+        "api/v1/login", data={"username": username, "password": password})
+
+    assert res.status_code == 404
+    # assert res.json().get('detail') == 'Invalid Credentials'
